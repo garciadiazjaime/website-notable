8f85e254b8bf71c01f16e2a8daa66fbee45a79d5
jaime <garciadiazjaime@gmail.com>
2016-04-20 13:46:57 -0500

    deploy

--- dist/server.js
+++ dist/server.js
@@ -156,12 +156,16 @@
 	  var sectionId = getSectionId(_sitemap2.default, url);
 	  if (sectionId) {
 	    var promises = [];
+	    console.log('apiUrl', apiUrl);
+	    console.log('sectionId', sectionId);
 	    promises.push(new Promise(function (resolve, reject) {
 	      (0, _restClient2.default)({
 	        path: apiUrl + 'api/block/?section_id=' + sectionId
 	      }).then(function (response) {
+	        console.log('here', response);
 	        resolve(response.entity);
 	      }, function (response) {
+	        console.log('error', response);
 	        reject(response);
 	      });
 	    }));
@@ -342,10 +346,10 @@
 	    env: 'SENDGRID_API_KEY'
 	  },
 	  apiUrl: {
-	    doc: 'Portfolio API URL',
+	    doc: 'API URL',
 	    format: String,
 	    default: '',
-	    env: 'API_PORTFOLIO'
+	    env: 'API_URL'
 	  },
 	});
 
