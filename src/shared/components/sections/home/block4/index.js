cba91c154fbfd446796f3db1aac25954acc4e9b1
alex-romanov <alejandro@mintitmedia.com>
2016-04-23 12:18:19 -0700

    home

--- src/shared/components/sections/home/block4/index.js
+++ src/shared/components/sections/home/block4/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
 
-// const style = require('./style.scss');
+const style = require('./style.scss');
 
 
 export default class Block4 extends React.Component {
 
   render() {
-    const { titles, images, buttons } = this.props.data;
-    return !_.isEmpty(this.props.data) ? (<div>
+    const { titles, buttons } = this.props.data;
+    const divStyle = {
+      backgroundImage: 'url(/images/img_showroom.jpg)',
+    };
+    return !_.isEmpty(this.props.data) ? (<div className={style.wrapper2} style={divStyle}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-6 col-xs-12">
-            <h2>{titles.title1}</h2>
-            <img src={images.image1.src} alt={images.image1.alt} />
-            <Link to={buttons.button1.href}>{buttons.button1.title}</Link>
+            <h2 className={style.mainTitle}>{titles.title1}</h2>
+            <Link className={style.button2} to={buttons.button1.href}>{buttons.button1.title}</Link>
           </div>
         </div>
       </div>
