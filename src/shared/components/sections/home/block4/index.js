5264925f58e0fef346ff6d93229fa3734e2cb78a
jaime <garciadiazjaime@gmail.com>
2016-04-28 07:36:11 -0500

    adjust server blocks fn

--- src/shared/components/sections/home/block4/index.js
+++ src/shared/components/sections/home/block4/index.js
@@ -1,21 +1,24 @@
+/* eslint max-len: [2, 500, 4] */
+
 import React from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
+import setImageAsBackground from '../../../../utils/setImageAsBackground';
 
-// const style = require('./style.scss');
+const style = require('./style.scss');
 
 
 export default class Block4 extends React.Component {
 
   render() {
-    const { titles, images, buttons } = this.props.data;
-    return !_.isEmpty(this.props.data) ? (<div>
+    const { titles, buttons, images } = this.props.data;
+    const divStyle = setImageAsBackground(images.image1);
+    return !_.isEmpty(this.props.data) ? (<div className={style.wrapper2} style={divStyle}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-6 col-xs-12">
-            <h2>{titles.title1}</h2>
-            <img src={images.image1.src} alt={images.image1.alt} />
-            <Link to={buttons.button1.href}>{buttons.button1.title}</Link>
+            <h2 className={style.mainTitle}>{titles.title1}</h2>
+            <Link className={style.button2} to={buttons.button1.href}>{buttons.button1.title}</Link>
           </div>
         </div>
       </div>
