332718a2d44f09a8030f7c3c2b275aabbd88a956
jaime <garciadiazjaime@gmail.com>
2016-04-28 14:48:13 -0500

    include image.src

--- src/shared/components/sections/products/block1/index.js
+++ src/shared/components/sections/products/block1/index.js
@@ -14,7 +14,7 @@ export default class Block1 extends React.Component {
     if (_.isArray(data) && data.length) {
       return data.map((item, index) => {
         const className = index === 0 ? 'active' : '';
-        const imgUrl = item.image; // && item.image.length ? item.image.replace('www.dropbox.com', 'dl.dropboxusercontent.com') : '/images/demo.png';
+        const imgUrl = item.image.length ? item.image.replace('www.dropbox.com', 'dl.dropboxusercontent.com') : '/images/demo.png';
 
         return (<div className={'item ' + className + ' ' + (style.item || '')} key={index}>
             <img src={imgUrl} alt={item.title} />
