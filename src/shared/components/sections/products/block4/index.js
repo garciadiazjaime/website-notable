da2108a3f01d4074593f2b7d5cc9a81a42bb51d3
alex-romanov <alejandro@mintitmedia.com>
2016-05-03 20:42:58 -0700

    issues 27 y 25 quedan irresolutos esperando comentario

--- src/shared/components/sections/products/block4/index.js
+++ src/shared/components/sections/products/block4/index.js
@@ -14,7 +14,7 @@ export default class Block4 extends React.Component {
     return !_.isEmpty(this.props.data) ? (<div>
       <div className={'container-fluid ' + style.wrapper}>
         <div className="row">
-          <div className="col-sm-6 col-xs-12">
+          <div className="col-sm-4 col-sm-offset-2 col-md-5 col-md-offset-1 col-xs-12">
             <h2 className={style.title}>{titles.title1}</h2>
             <p className={style.paragraph} dangerouslySetInnerHTML={sanitizeUtil(paragraphs.paragraph1)} />
             <Link className={style.button} to={buttons.button1.href}>{buttons.button1.title}</Link>
